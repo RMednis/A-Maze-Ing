4350a021faffcd60d0e430e9996d14d5967a3121
Aleksey Karelin <64694787+Wolferado@users.noreply.github.com>
2021-12-22 14:06:59 +0200

    Update SolveClass.java

--- dip107/SolveClass.java
+++ dip107/SolveClass.java
@@ -1,5 +1,30 @@
 package dip107;
 
 public class SolveClass {
-    
+    // For everyone to dive into
+    /* TODO: Create an algorithm which takes the path of zeros upon encountering the dead-end.
+    *  When encountering a multiple paths, randomly choose the path that will be taken.
+    *  Reset from (0, 0), if there is no more available paths to go (no zeros near, not end of the maze, dead-end encountered). */
+    private static void RandomSolve(int[][] arrayToSolve) {
+
+    }
+
+    // For Aleksey and Daniil
+    /* TODO: Create an algorithm that takes the path of zeros upon encountering the dead-end. 
+    *  When encountering a multiple paths, randomly choose the path that will be taken.
+    *  Reset from (0, 0), if there is no more available paths to go.
+    *  Use received information about failed paths and choose other path to go randomly. */
+    private static void TryOutEverythinhSolve(int[][] arrayToSolve) {
+
+    }
+
+    // For Alexander and Reinis
+    /* TODO: Create an algorithm that takes the path of zeros upon encountering the dead-end.
+    *  When encountering a multiple paths, randomly choose the path that will be taken.
+    *  If there is no more available paths to go, return to the previous intersection of multiple paths.
+    *  Register the tried paths to use this information and choose other path. 
+    */
+    private static void RealLifeApproachSolve(int[][] arrayToSolve) {
+
+    }
 }
