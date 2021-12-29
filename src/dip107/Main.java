4cfff83a69424e460211abebeb693e711e98b89c
Reinis Mednis <reinis@mednis.id.lv>
2021-12-29 13:12:27 +0200

    Moved input checks for integers and yes/no answers to separate functions.
    This reduces the amount of code reuse, and is just a better idea in general.

--- src/dip107/Main.java
+++ src/dip107/Main.java
@@ -7,69 +7,36 @@ public class Main {
     public static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int r = 0;
-        int k = 0;
+        int r;
+        int k;
 
         System.out.print("row count: ");
-
-        if (sc.hasNextInt()) {
-            r = sc.nextInt();
-        } else {
-            System.out.println("input error");
-            System.exit(1);
-        }
+        r = numberCheck(sc);
 
         System.out.print("column count: ");
-
-        if (sc.hasNextInt()) {
-            k = sc.nextInt();
-        } else {
-            System.out.println("input error");
-            System.exit(1);
-        }
-
-        char answer = 'x';
+        k = numberCheck(sc);
 
         sc.nextLine();
-
         System.out.print("Autofill the maze? (y/n) ");
 
-        if (sc.hasNextLine()) {
-            answer = sc.nextLine().charAt(0);
-        } else {
-            System.out.println("input error");
-            System.exit(1);
-        }
+        boolean answer = yesNoCheck(sc);
 
         LabyrinthClass labyrinth = new LabyrinthClass(r, k);
         SolveClass solver = new SolveClass();
 
-        if (answer == 'n') {
+        if (!answer) {
             for (int i = 0; i < r; i++) {
                 for (int j = 0; j < k; j++) {
-                    if (sc.hasNextInt()) {
-                        labyrinth.array[i][j] = sc.nextInt();
-                    } else {
-                        System.out.println("input error");
-                        System.exit(1);
-                    }
+                    labyrinth.array[i][j] = numberCheck(sc);
                 }
             }
             // Setting up start and ending
             labyrinth.array[0][0] = 0;
             labyrinth.array[r - 1][k - 1] = 0;
-        } else if (answer == 'y') {
-            int methodNum = 0;
+        } else {
             System.out.print("Maze generation method (1-2): ");
 
-            if (sc.hasNextInt()) {
-                methodNum = sc.nextInt();
-            } else {
-                System.out.println("input error");
-                System.exit(1);
-            }
-
-            switch (methodNum) {
+            switch (numberCheck(sc)) {
                 case 1 -> labyrinth.GenerateLabyrinth(1);
                 case 2 -> labyrinth.GenerateLabyrinth(2);
                 default -> {
@@ -83,20 +50,14 @@ public class Main {
             labyrinth.array[r - 1][k - 1] = 0;
 
 
-            System.out.print("Pretty print the maze? (y/n)");
+            System.out.print("Pretty print the maze? (y/n) ");
 
-            if (sc.hasNextLine()) {
-                answer = sc.next().charAt(0);
-            } else {
-                System.out.println("input error");
-                System.exit(1);
-            }
-
+            answer = yesNoCheck(sc);
 
             for (int i = 0; i < r; i++) {
                 for (int j = 0; j < k; j++) {
 
-                    if (answer == 'y') { // Pretty print the maze using block characters
+                    if (answer) { // Pretty print the maze using block characters
                         if (labyrinth.array[i][j] == 1) {
                             System.out.print("▉");
                         } else {
@@ -116,16 +77,7 @@ public class Main {
 
             System.out.print("method number (1-3): ");
 
-            if (sc.hasNextInt()) {
-                methodNum = sc.nextInt();
-            } else {
-                System.out.println("input error");
-                System.exit(1);
-            }
-
-            System.out.println("results:");
-
-            switch (methodNum) {
+            switch (numberCheck(sc)) {
                 case 1 -> solver.RandomSolve(labyrinth);
                 case 2 -> solver.TryOutEverythingSolve(labyrinth);
                 case 3 -> solver.RealLifeApproachSolve(labyrinth);
@@ -134,6 +86,9 @@ public class Main {
                     System.exit(1);
                 }
             }
+
+            System.out.println("results:");
+
 
             // Printing the maze once it's been solved (or not)
             if (labyrinth.solved) {
@@ -156,6 +111,35 @@ public class Main {
             }
 
 
+        }
+    }
+
+    public static boolean yesNoCheck(Scanner sc) {
+        if (sc.hasNextLine()) {
+            char input = sc.next().charAt(0);
+            switch (input) {
+                case 'y': return true;
+                case 'n': return false;
+                default: {
+                    System.out.println("input error");
+                    System.exit(1);
+                    return false;
+                }
+            }
+        } else {
+            System.out.println("input error");
+            System.exit(1);
+            return false;
+        }
+    }
+
+    public static int numberCheck(Scanner sc) {
+        if (sc.hasNextInt()) {
+            return sc.nextInt();
+        } else {
+            System.out.println("input error");
+            System.exit(1);
+            return 0;
         }
     }
 }
